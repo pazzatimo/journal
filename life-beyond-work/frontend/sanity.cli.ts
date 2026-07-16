f37508b0b7f6b801cf7e16a917272214fcd566db
Pazza <t.pazza@protonmail.ch>
2026-07-16 14:56:50 +0300

    Add appId to sanity.cli.ts for smoother deployments

---
+++ life-beyond-work/frontend/sanity.cli.ts
@@ -0,0 +1,12 @@
+echo 'import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "3ojsturu",
+    dataset: "production",
+  },
+  studioHost: "timopazza",
+  deployment: {
+    appId: "os5yncsnalo5tb22rhmj7wcq",
+  },
+});' > sanity.cli.ts 
